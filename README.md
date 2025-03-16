8f7a6604e3bcc3826cf8d295c3fd8540d2c7ea9b
0xMere <merenda.s.mattia@gmail.com>
2025-03-16 11:53:19 +0100

    Added readme, bytecodes, ABIs, JSONs, requirements

---
+++ README.md
@@ -0,0 +1,23 @@
+# Setup Instructions
+
+This project requires a Python virtual environment to manage dependencies. Follow these steps to set up and install the required modules.
+
+## 🔧 Setup
+1. Create a virtual environment:
+```bash
+python -m venv venv
+```
+
+2. Activate the virtual environment:
+```bash
+source venv/bin/activate
+```
+
+3. Install dependencies:
+```bash
+pip install -r requirements.txt
+```
+
+## ✅ Ready to Use
+
+Once the setup is complete, you can run the scripts as needed.
--- compile-smart-contracts.py
+++ compile-smart-contracts.py
@@ -364,20 +364,9 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    if args.cross_chain_xguard:
-        compile_bridge('cross-chain/XGuard/QBridge') # QBridge
-        compile_bridge('cross-chain/XGuard/MeterBridge') # MeterBridge
-
-    if args.manual:
-        # Test ThorChain Bridge
-        extract_solidity_versions(src_folder='./cross-chain/THORChain-bridge/source-code',
-                                  output_csv='./cross-chain/THORChain-bridge/source-code/version.csv')
-        compile_solidity_sources_with_different_version(source_dir='./cross-chain/THORChain-bridge/source-code',
-                                                        json_dir='./cross-chain/THORChain-bridge/json',
-                                                        version_file='./cross-chain/THORChain-bridge/source-code/version.csv')
-        extract_and_save_longest_bytecode(bytecode_dir='./cross-chain/THORChain-bridge/bytecode/',
-                                          json_dir='./cross-chain/THORChain-bridge/json',
-                                          abi_dir='./cross-chain/THORChain-bridge/abi/')
+    # if args.cross_chain_xguard:
+    #     compile_bridge('cross-chain/XGuard/QBridge') # QBridge
+    #     compile_bridge('cross-chain/XGuard/MeterBridge') # MeterBridge
 
     if args.solidifi:
         compile_solidity_sources('./solidifi/reentrancy/source-code',
